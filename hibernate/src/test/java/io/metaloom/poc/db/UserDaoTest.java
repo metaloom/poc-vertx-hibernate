c3bce91c8c3b6427a19f330ecf71bbdd895e3d2c
Johannes Schüth <git@jotschi.de>
2021-11-28 10:32:56 +0100

    Use java configuration for DAO test as well

--- hibernate/src/test/java/io/metaloom/poc/db/UserDaoTest.java
+++ hibernate/src/test/java/io/metaloom/poc/db/UserDaoTest.java
@@ -1,52 +1,50 @@
 package io.metaloom.poc.db;
 
-import static javax.persistence.Persistence.createEntityManagerFactory;
-
+import org.hibernate.reactive.session.impl.ReactiveSessionFactoryImpl;
 import org.hibernate.reactive.stage.Stage.SessionFactory;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import io.metaloom.poc.db.impl.PocGroupDaoImpl;
+import io.metaloom.poc.db.hib.HibernateUtil;
 import io.metaloom.poc.db.impl.PocUserDaoImpl;
+import io.metaloom.poc.option.DatabaseOptions;
 
 public class UserDaoTest extends AbstractDaoTest {
 
+	private SessionFactory factory;
+
+	@Before
+	public void setupHibernate() {
+		DatabaseOptions options = container.getOptions();
+		ReactiveSessionFactoryImpl rxFactory = (ReactiveSessionFactoryImpl) HibernateUtil.sessionFactory(options.getJdbcUrl(), options.getUsername(),
+			options.getPassword());
+		factory = rxFactory.unwrap(SessionFactory.class);
+	}
+
+	@After
+	public void cleanup() {
+		if (factory != null) {
+			factory.close();
+		}
+	}
+
 	@Test
 	public void testUserDao() {
-
-		// obtain a factory for reactive sessions based on the
-		// standard JPA configuration properties specified in
-		// resources/META-INF/persistence.xml
-		String[] args = {};
-		SessionFactory factory = createEntityManagerFactory(persistenceUnitName(args)).unwrap(SessionFactory.class);
-
 		PocUserDao userDao = new PocUserDaoImpl(factory);
 		PocUser user1 = userDao.createUser("Iain M. Banks").blockingGet();
 		// PocUser user2 = new PocUserImpl("Neal Stephenson");
 		// PocUser user3 = new PocUserImpl("Arthur C. Clarke");
-
 		// groupDao.addUserToGroup(group1, user1);
 		// groupDao.addUserToGroup(group1, user2);
-
 	}
 
-	@Test
-	public void testUserGroup() {
-		String[] args = {};
-		SessionFactory factory = createEntityManagerFactory(persistenceUnitName(args)).unwrap(SessionFactory.class);
-		PocGroupDao groupDao = new PocGroupDaoImpl(factory);
-		PocGroup group1 = groupDao.createGroup("guests").blockingGet();
-		PocGroup group2 = groupDao.createGroup("admins").blockingGet();
-		PocGroup group3 = groupDao.createGroup("editors").blockingGet();
-	}
+//	@Test
+//	public void testUserGroup() {
+//		PocGroupDao groupDao = new PocGroupDaoImpl(factory);
+//		PocGroup group1 = groupDao.createGroup("guests").blockingGet();
+//		PocGroup group2 = groupDao.createGroup("admins").blockingGet();
+//		PocGroup group3 = groupDao.createGroup("editors").blockingGet();
+//	}
 
-	/**
-	 * Return the persistence unit name to use in the example.
-	 *
-	 * @param args
-	 *            the first element is the persistence unit name if present
-	 * @return the selected persistence unit name or the default one
-	 */
-	public static String persistenceUnitName(String[] args) {
-		return args.length > 0 ? args[0] : "postgresql-example";
-	}
 }
